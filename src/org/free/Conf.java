c631a28d4dd5bdf3f9021319629f54bf7a584831
bellcliff <cliff_bell.cn@hotmail.com>
2011-08-14 00:40:18 +0800

    Change-Id: I3f5cf52c680f488ed6446ddb1b18da664db34fb8

--- src/org/free/Conf.java
+++ src/org/free/Conf.java
@@ -3,4 +3,14 @@ package org.free;
 public class Conf {
 
 	static boolean isDouble = false;
+
+	/**
+	 * 鱼饵
+	 */
+	static boolean enBait = true;
+	
+	/**
+	 * 鱼饵持续时间
+	 */
+	static int inBait = 15;
 }
