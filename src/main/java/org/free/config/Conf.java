d0aee2243fa16fb98390387f10ecec09d3ce075c
Bo Yang <Bo.Yang@aexp.com>
2016-07-31 16:08:57 -0400

    adjust the scan position

--- src/main/java/org/free/config/Conf.java
+++ src/main/java/org/free/config/Conf.java
@@ -5,11 +5,11 @@ public interface Conf {
 	
 	boolean DEBUG = false;
 
-	int scanLeft = 0;
-	int scanTop = -10;
+	int scanLeft = -10;
+	int scanTop = -20;
 	int scanWidth = 40;
 	int scanHeight = 40;
-	int scanLight = 20;
+	int scanLight = 25;
 	int scanBlock = 4;
 	int scanInterval = 100;
 	EditableConf EDITABLE_CONF = new EditableConf();
